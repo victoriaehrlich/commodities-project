8068ac1fcc7803dc2facb030903b6b8bb5434079
Sam <70585138+sambloom1@users.noreply.github.com>
2026-04-15 17:35:38 +0100

    Update bar-chart with the world map. Annotated code with explanations and added some overall separation notation

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -1,24 +1,38 @@
 const barBlue = "#4682b4";
 const middleEast = new Set(["Iraq", "Saudi Arabia", "Oman", "Iran"]);
 
-d3.csv("data/oil-gdp.csv", d => ({
+
+// A promise all to handle the loading of two datasets
+Promise.all([
+  d3.json("./data/world.json"), // first dataset becomes the "world" argument, second becomes oilData 
+  d3.csv("data/oil-gdp.csv", d => ({
   Country: d.Entity,
   Oil_rent: +d.of_GDP,
-})).then(data => {
-
-  // Sort all countries by Oil_rent descending, then keep only the top 10.
-  // .slice() does not mutate the original array.
-  const top10 = data
+        })
+    ) // closing bracket for oilData
+  ]
+).then(([world, oilData]) => { // loading both parameters
+  
+const top10 = oilData // Idea potentially that this could be dynamic based on a slider - would be an event handler type input like a button
     .sort((a, b) => b.Oil_rent - a.Oil_rent)
     .slice(0, 10);
 
-  createVis(top10);
+  createVis(world, top10);
+})
+.catch(error => {
+  console.error("Error loading JSON files:", error);
 });
 
-function createVis(rent) {
+function createVis(world, rent) {
 
   const labelColWidth = 90;
 
+  // Scales
+  const CountryColourScale = d3.scaleSequential() // Colour scale for used for both the bars and the map
+          .domain([10,60])
+          .interpolator(d3.interpolateYlGnBu); // // Used to tailor the colour specturm, this is usually the default one. could interpolate just the blue
+
+  
   const svg = d3.select("#bar-chart")
     .append("svg")
     .attr("viewBox", `0 0 ${width} ${height}`);
@@ -31,6 +45,10 @@ function createVis(rent) {
     .domain(rent.map(d => d.Country))
     .range([0, innerHeight])
     .paddingInner(0.15);
+
+/*----------------------------- */  
+
+// Bar Chart Visual
 
   const fmt = d3.format(".1f");
 
@@ -60,7 +78,7 @@ function createVis(rent) {
         .attr("x", labelColWidth)
         .attr("width", d => xScale(d.Oil_rent) - labelColWidth)
         .attr("height", yScale.bandwidth())
-        .attr("fill", barBlue)
+        .attr("fill", d=> CountryColourScale(d.Oil_rent))
         .attr("fill-opacity", d => middleEast.has(d.Country) ? 1 : 0.4)
         .attr("rx", 2)
         .attr("ry", 2);
@@ -90,4 +108,40 @@ function createVis(rent) {
     .attr("dominant-baseline", "middle")
     .attr("text-anchor", "start");
 
+
+ /*----------------------------- */  
+  // World Map Visual   
+  const worldMap = svg.append("g")
+    .attr("class", "world")
+    .attr("transform", `translate(500,0)`); // Position at the start of the hal way point on the SVG
+
+
+  const projectionMercator = d3.geoMercator() 
+                    .translate([width/4.5, height/1.5]) // Translates the centre of the map e.g. 1000/4.5 = 220 but its from the starting point of 500 of the grouped element
+                    .scale(100); // determines how zoomed in the map in combination with the size of grouped element or svg 
+
+
+  const geoPathGenerator = d3.geoPath() // intitialises a path generator using the function d3.geoPath(). Same process as a line or area generator
+    .projection(projectionMercator); // pass the projectionMercator declared above to the projection() accessor function.
+
+
+  world.features.forEach(country=> {  // looping through each feature and saying that's country
+    const props = country.properties;  // naming a variable props that captures the little object(like a dataset) at the end of each Country row called properties - "properties":{"name":"Bahrain"},"id":"BH"},
+    const match = rent.find(r => r.Country === props.name); // in the loop we then look to find in the rent data if the current country being looped matches. If it does thats then assigned to match constant 
+    props.Oil_rent = match ? match.Oil_rent : 0; // Finally here we go 2 lines up when we created props and add another property to that called Oil_rent
+    // "properties":{"name":"Bahrain"},{Oil_rent: 50}, "id":"BH"},
+    });
+
+
+worldMap.selectAll(".country-path")
+    .data(world.features) // I missed out the .features here which is why it didnt load, it has to be world.features as that's what the geopath generator is looking to plot 
+    .join("path")
+        .attr("class", "country-path")
+        .attr("d", geoPathGenerator) // drawing a path using the generator above. All the cordinaes part - "geometry":{"type":"MultiPolygon","coordinates":[[[
+        .attr("fill", d =>  d.properties.Oil_rent > 0 ? CountryColourScale(d.properties.Oil_rent) : "#ccc" ) // Fill based on the condition that oils_rent is bigger than 0, all countries apart from the top 10 will be 0 because of the const match part in the above loop
+        .attr("fill-opacity", 0.8)
+        .attr("stroke", "black")
+        .attr("stroke-opacity", 1);
+
 }
+
